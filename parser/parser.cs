8217c586b1ccc1f6f418ff78aa15852886796558
Andrwe Lord Weber <lord-andrwe-weber@renona-studios.org>
2009-05-12 13:57:33 +0200

    add parser files with class structure

---
+++ parser/parser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace parser
+{
+    public class parser
+    {
+        public void readFile(string file)
+        {
+        }
+        public void writeFile(string file)
+        {
+        }
+        public string getValue(string option)
+        {
+        }
+        public void setValue(string option, string value)
+        {
+        }
+    }
+}
