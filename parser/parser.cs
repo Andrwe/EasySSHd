5c21cb0117b233c93c9f29fd3ffc01b83dddc816
Andrwe Lord Weber <lord-andrwe-weber@renona-studios.org>
2009-05-15 20:26:59 +0200

    class Array fileContent implemented function readFile implemented

--- parser/parser.cs
+++ parser/parser.cs
@@ -1,20 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace parser
 {
     public class parser
     {
+        private ArrayList fileContent = new ArrayList();
+
         public void readFile(string file)
         {
+            string fileLine = "";
+            StreamReader fileStr = new StreamReader(file);
+            while (fileLine != null)
+            {
+                fileLine = fileStr.ReadLine();
+                if (fileLine != null)
+                    fileContent.Add(fileLine);
+            }
+            fileStr.Close();
         }
         public void writeFile(string file)
         {
         }
         public string getValue(string option)
         {
+            string value = "test";
+            return value;
         }
         public void setValue(string option, string value)
         {
