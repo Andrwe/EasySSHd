db0c55e0215a4ca0e0e540283d234a924fe63ba6
Andrwe Lord Weber <lord-andrwe-weber@renona-studios.org>
2009-05-18 18:24:23 +0200

    set comment for each function exception handler for readFile implemented function writeFile, setValue with exceptions implemented Bug fix: get comment after value of an option cut a part of the value if no space is between value and comment.

--- parser/parser.cs
+++ parser/parser.cs
@@ -8,12 +8,34 @@ namespace parser
 {
     public class parser
     {
+        /*
+         * Global Array for working with content of a file.
+         */
         private ArrayList fileContent = new ArrayList();
 
-        public void readFile(string file)
+        /*
+         * Open file, read content and save read content into "ArrayList fileContent"
+         * Exception:
+         * 0 - all fine
+         * 1 - file not found
+         * 2 - file not readable
+         */
+        public int readFile(string file)
         {
             string fileLine = "";
-            StreamReader fileStr = new StreamReader(file);
+            StreamReader fileStr;
+            try
+            {
+                fileStr = new StreamReader(file);
+            }
+            catch (FileNotFoundException)
+            {
+                return 1;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return 2;
+            }
             while (fileLine != null)
             {
                 fileLine = fileStr.ReadLine();
@@ -21,40 +43,104 @@ namespace parser
                     fileContent.Add(fileLine);
             }
             fileStr.Close();
+            return 0;
         }
-        public void writeFile(string file)
+        /*
+         * Open file for writing and write lines from "ArrayList fileContent" into "file".
+         * Exceptions:
+         * 0 - all fine
+         * 1 - file not writable
+         */
+        public int writeFile(string file)
         {
+            string fileLine = "";
+            StreamWriter fileStr;
+            try
+            {
+                fileStr = new StreamWriter(file, false);
+            }
+            catch(UnauthorizedAccessException)
+            {
+                return 1;
+            }
+
+            foreach (string line in fileContent)
+            {
+                fileLine = line;
+                if (fileLine != null)
+                {
+                    fileStr.WriteLine(line);
+                }
+            }
+            fileStr.Close();
+
+            return 0;
         }
+        /*
+         * Read value of given option from "fileContent"
+         * Exception:
+         * "value" - value of option
+         * "" - no value
+         */
         public string getValue(string option)
         {
             string value = "";
-            int positionComment;
-            int positionOption;
-            value = searchLine(option);
-            if (value == "")
+            if (option != "")
             {
-                value = "Option does not exist.";
-            }
-            else
-            {
-                positionComment = value.IndexOf('#');
-                if (positionComment > -1)
+                int positionComment;
+                int positionOption;
+                int linePosition;
+                linePosition = searchLine(option);
+                if (linePosition > -1 && linePosition < fileContent.Count)
                 {
-                    value = value.Substring(0, positionComment - 1);
+                    value = fileContent[linePosition].ToString();
                 }
-                value.Trim();
-                positionOption = value.IndexOf(' ');
-                value = value.Substring(positionOption + 1, value.Length - 1 - positionOption);
+                if (value != "")
+                {
+                    positionComment = value.IndexOf('#');
+                    if (positionComment > -1)
+                    {
+                        value = value.Substring(0, positionComment);
+                    }
+                    value.Trim();
+                    positionOption = value.IndexOf(' ');
+                    value = value.Substring(positionOption + 1, value.Length - 1 - positionOption);
+                }
             }
             return value;
         }
+        /*
+         * Set given value of given option into "fileContent".
+         */
         public void setValue(string option, string value)
         {
+            if (option != "" && value != "")
+            {
+                int linePosition;
+                string line;
+                string lineComment = "";
+                int commentPosition;
+                linePosition = searchLine(option);
+                line = fileContent[linePosition].ToString();
+                commentPosition = line.IndexOf('#');
+                if (commentPosition > -1)
+                {
+                    lineComment = line.Substring(commentPosition, line.Length - 1 - commentPosition);
+                }
+                fileContent[linePosition] = option + " " + value + lineComment;
+            }
         }
-        private string searchLine(string option)
+        /*
+         * Search in "ArrayList fileContent" for given option and return line where it was found.
+         * Exception:
+         * line - line which contains given option
+         * -1   - option not found
+         */
+        private int searchLine(string option)
         {
             int position;
             int positionComment;
+            int linePosition = 0;
             foreach (string line in fileContent)
             {
                 line.Trim();
@@ -62,14 +148,16 @@ namespace parser
                 position = line.IndexOf(option);
                 if (positionComment == 0 || (positionComment < position && positionComment != -1))
                 {
+                    linePosition++;
                     continue;
                 }
                 if (position == 0)
                 {
-                    return line;
+                    return linePosition;
                 }
+                linePosition++;
             }
-            return "";
+            return -1;
         }
     }
 }
